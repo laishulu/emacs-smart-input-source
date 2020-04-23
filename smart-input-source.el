228f640469bcc32573ebe334eee68adf7145824f
Mao <code@mao.com>
2020-04-23 12:12:14 +0800

    improve comments

--- smart-input-source.el
+++ smart-input-source.el
@@ -27,7 +27,7 @@
 
 ;;; Code:
 
-;; `define-namespace' is autoloaded, so there's no need to require `names'.
+;; ~define-namespace~ is autoloaded, so there's no need to require ~names~.
 ;; However, requiring it here means it will also work for people who don't
 ;; install through package.el.
 (eval-when-compile (require 'names))
@@ -105,17 +105,17 @@ Should accept a string which is the id of the input source.")
 (make-variable-buffer-local (quote -ism))
 
 (defun -string-match-p (regexp str &optional start)
-  "Robust wrapper of `string-match-p'.
+  "Robust wrapper of ~string-match-p~.
 
 Works when REGEXP or STR is not a string REGEXP, STR, START all has the same
-meanings as `string-match-p`."
+meanings as ~string-match-p~."
   (and (stringp regexp)
        (stringp str)
        (string-match-p regexp str start)))
 
 (cl-defstruct back-detect ; result of backward detect
   to ; point after first non-blank char in the same line
-  char ; first non-blank char at the same line (just before position `to`)
+  char ; first non-blank char at the same line (just before position ~to~)
   cross-line-to ; point after first non-blank char cross lines
   cross-line-char ; first non-blank char cross lines before the current position
   )
@@ -139,7 +139,7 @@ meanings as `string-match-p`."
 
 (cl-defstruct fore-detect ; result of forward detect
   to ; point before first non-blank char in the same line
-  char ; first non-blank char at the same line (just after position `to`)
+  char ; first non-blank char at the same line (just after position ~to~)
   cross-line-to ; point before first non-blank char cross lines
   cross-line-char ; first non-blank char cross lines after the current position
   )
@@ -220,7 +220,7 @@ meanings as `string-match-p`."
       OTHER))))
 
 (defun -mk-get-input-source-fn ()
-  "Make a function to be bound to `do-get-input-source`."
+  "Make a function to be bound to ~do-get-input-source~."
   (when -ism
     (if (equal -ism ISM-EMP)
         #'mac-input-source
@@ -228,7 +228,7 @@ meanings as `string-match-p`."
         (string-trim (shell-command-to-string -ism))))))
 
 (defun -mk-set-input-source-fn ()
-  "Make a function to be bound to `do-set-input-source`."
+  "Make a function to be bound to ~do-set-input-source~."
   (when -ism
     (if (equal -ism ISM-EMP)
         (lambda (source) (mac-select-input-source source))
@@ -258,11 +258,11 @@ meanings as `string-match-p`."
 (define-minor-mode mode
   "Switch input source smartly.
 
-For GUI session of `emacs mac port`, use native API to select input source
-for better performance.
-If `emacs mac port` is unavailable, or in terminal session, use external ism
-tool to select input source.
-If no ism found, then do nothing."
+- For GUI session of ~emacs mac port~, use native API to select input source
+  for better performance.
+- If ~emacs mac port~ is unavailable, or in terminal session, use external ism
+  tool to select input source.
+- If no ism found, then do nothing."
   :init-value nil
   (when (and (string= (window-system) "mac")
              (fboundp 'mac-input-source))
@@ -299,7 +299,9 @@ If no ism found, then do nothing."
 (defun check-to-activate-overlay()
   "Check whether to activate the inline english region overlay.
 
-return `t` after activating the overlay."
+Check the context to determine whether the overlay should be activated or not,
+if the answer is yes, then activate the /inline english region/, set the
+input source to English, and then return ~t~."
   (unless (or (not mode)
               (overlayp -inline-overlay)
               (button-at (point))
@@ -320,7 +322,10 @@ return `t` after activating the overlay."
         t))))
 
 (defun do ()
-  "Do the smart input source."
+  "Do the smart input source switching.
+
+Do ~smart-input-source-check-to-activate-overlay~ as
+well as ~smart-input-source-follow-context~. "
   (unless (check-to-activate-overlay)
     (follow-context)))
 
@@ -332,12 +337,12 @@ return `t` after activating the overlay."
         (-set-input-source context)))))
 
 (defun set-input-source-english ()
-  "Set input source to `english-input-source`."
+  "Set input source to ~english-input-source~."
   (when mode
     (-set-input-source ENGLISH)))
 
 (defun set-input-source-other ()
-  "Set input source to `other-input-source`."
+  "Set input source to ~other-input-source~."
   (-set-input-source OTHER))
 
 ;;
